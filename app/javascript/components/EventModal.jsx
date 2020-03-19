b791dce6b64a6e7aa56e2e1f55b35475e705d291
renebreslauer <renebreslauer@me.com>
2020-03-18 20:57:30 -0400

    modal styling

--- app/javascript/components/EventModal.jsx
+++ app/javascript/components/EventModal.jsx
@@ -1,28 +1,53 @@
 import React from 'react'
 
-const EventModal = (props) => {
-  return(
-    <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog" role="document">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">{props.name}</h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close" onClick={props.closeModalToggle}>
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
-      <div class="modal-body">
-          {props.about}
-      </div>
-      <div>
-        Stuff goes here!
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-dismiss="modal" onClick={props.closeModalToggle}>Close</button>
+const EventModal = props => {
+  return (
+    <div
+      class="modal fade"
+      id="exampleModal"
+      tabindex="-1"
+      role="dialog"
+      aria-labelledby="exampleModalLabel"
+      aria-hidden="true"
+    >
+      <div class="modal-dialog" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <div class="modal-header-top">
+              <h5 class="modal-title" id="exampleModalLabel">
+                {props.date}
+              </h5>
+              <button
+                type="button"
+                class="close"
+                data-dismiss="modal"
+                aria-label="Close"
+                onClick={props.closeModalToggle}
+              >
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <div class="modal-header-mid">{props.name}</div>
+            <div class="modal-header-bottom">Bookmark event</div>
+          </div>
+          <div class="modal-body">
+            <div class="modal-col-1">{props.about}</div>
+            <div class="modal-col-2">date, location, info</div>
+          </div>
+          <div>Stuff goes here!</div>
+          <div class="modal-footer">
+            <button
+              type="button"
+              class="btn btn-secondary"
+              data-dismiss="modal"
+              onClick={props.closeModalToggle}
+            >
+              Close
+            </button>
+          </div>
+        </div>
       </div>
     </div>
-  </div>
-</div>
   )
 }
 
